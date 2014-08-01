a79605d3ecd3b05e79eb90af370e15f65d3f20c4
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-01 13:35:08 -0700

    Update

--- nominate.php
+++ nominate.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if (!isset($_REQUEST('nominator')) ||
-	!isset($_REQUEST('name')) ||
-	!isset($_REQUEST('position')) ||
-	!isset($_REQUEST('team'))) {
+if (!isset($_REQUEST['nominator']) ||
+	!isset($_REQUEST['name']) ||
+	!isset($_REQUEST['position']) ||
+	!isset($_REQUEST['team'])) {
 	header("HTTP/1.1 400 Bad Request");
 	exit();
 }
