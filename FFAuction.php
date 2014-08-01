3f40d599dac630979a7aef2c6cc5d07796a8f82d
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-01 12:34:45 -0700

    Update

--- FFAuction.php
+++ FFAuction.php
@@ -1,15 +1,16 @@
 <?php
 session_start();
 
-if (!isset($_POST['name']) ||
-	!isset($_POST['commish'])) {
-
-  header('Location: FFAuction-login.php');
-  exit();
+if (!isset($_SESSION['name']) && !isset($_POST['name'])) {
+	header('Location: FFAuction-login.php');
+  	exit();
 }
 
-$_SESSION['name'] = $_POST['name'];
-if ($_POST['commish'] == 'yes') {
+if (isset($_POST['name'])) {
+	$_SESSION['name'] = $_POST['name'];
+}
+
+if (isset($_POST['commish']) && $_POST['commish'] == 'yes') {
 /*	print("Yes"); */
 	include 'FFAuctionCommish.php';
 } else {
