f61a8712332d0dcfd76d5428eae6fbeea4eb68d4
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-01 13:33:04 -0700

    Update

---
+++ nominate.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+if (!isset($_REQUEST('nominator') ||
+	!isset($_REQUEST('name')) ||
+	!isset($_REQUEST('position')) ||
+	!isset($_REQUEST('team'))) {
+	header("HTTP/1.1 400 Bad Request");
+	exit();
+}
+
+
