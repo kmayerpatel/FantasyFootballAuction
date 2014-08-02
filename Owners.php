b36763697924d8fc49780c1c7412ab5dfcd6f652
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-02 08:38:55 -0700

    update

--- Owners.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$owners = array('Ketan', 'Jamo', 'Forbes', 'CG', 'Z', 'Los', 'Elder', 'Singer', 'Vince', 
-	            "O'Malley", 'Rich', 'Terrence');
-
-?>
--- Teams.php
+++
@@ -1,4 +0,0 @@
-<?php
-$teams = array('ARI','ATL','BAL','BUF','CAR','CHI','CIN','CLE',
-	'DAL','DEN','DET','GB','HOU','IND','JAC','KC','MIA','MIN',
-	'NE','NO','NYG','NYJ','OAK','PHI','PIT','SAN','SF','SEA','STL','TB','TEN','WAS');
