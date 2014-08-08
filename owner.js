dcf5b5c9e4389ba1d634c811413c589212dd5038
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-08 11:20:05 -0400

    update

--- owner.js
+++ owner.js
@@ -9,7 +9,7 @@ Owner.MIN_ROSTER_SIZE = 12;
 Owner.SALARY_CAP = 100;
 
 Owner.lookup = function(name) {
-    for (var i=0; i<Owner.owners; i++) {
+    for (var i=0; i<Owner.owners.length; i++) {
         if (Owner.owners[i].name == name) {
             return Owner.owners[i];
         }
@@ -30,25 +30,25 @@ Owner.prototype.maxBid = function() {
     var balance = Owner.SALARY_CAP;
 
     for (var i=0; i<this.roster.length; i++) {
-	   balance -= this.roster[i].price;
+        balance -= this.roster[i].price;
     }
 
     if (Owner.MIN_ROSTER_SIZE > this.roster.length+1) {
-	   return balance - (Owner.MIN_ROSTER_SIZE - this.roster.length - 1);
+        return balance - (Owner.MIN_ROSTER_SIZE - this.roster.length - 1);
     } else 
-	   return balance;
-    }
+    return balance;
+}
 }
 
 Owner.prototype.removeFromRoster = function(transaction) {
     var new_roster = [];
     for (var i = 0; i<this.roster.length; i++) {
-	if (this.roster[i] != transaction) {
-	    new_roster.push(this.roster[i]);
-	}
-    }
-    this.roster = new_roster;
-    this.notifyObservers();
+       if (this.roster[i] != transaction) {
+           new_roster.push(this.roster[i]);
+       }
+   }
+   this.roster = new_roster;
+   this.notifyObservers();
 }
 
 Owner.prototype.registerRosterObserver = function(observer) {
@@ -57,6 +57,6 @@ Owner.prototype.registerRosterObserver = function(observer) {
 
 Owner.prototype.notifyObservers = function() {
     for (var i=0; i<this.rosterObservers.length; i++) {
-	   this.rosterObservers[i].rosterChange(this);
+        this.rosterObservers[i].rosterChange(this);
     }
 }
