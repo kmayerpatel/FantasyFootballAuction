287ead6f77b44df1bd0d7ee223cd4eadb845b042
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-09 16:34:14 -0400

    update

--- ffauction-main.js
+++ ffauction-main.js
@@ -67,6 +67,11 @@ $(document).ready(function() {
                                                   auction_status.current_auction.nomination.team),
                                         auction_status.current_auction.timestamp);
             auction_ui.setAuction(auction);
+
+            for (var i=0; i<Owner.owners.length; i++) {
+                Owner.owners[i].clear();
+            }
+            
             for (var i=0; i<auction_status.current_auction.bids.length; i++) {
                 var next_bid = auction_status.current_auction.bids[i];
                 auction.confirmBid(Owner.lookup(next_bid.bidder), next_bid.bid, auction.timestamp);
