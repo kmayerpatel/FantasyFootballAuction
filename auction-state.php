d959aeb2ca77432bbd123b4e7abe77c3d48e1325
Ketan Mayer-Patel <kmp@cs.unc.edu>
2014-08-09 22:42:23 -0400

    update

--- auction-state.php
+++ auction-state.php
@@ -1,8 +1,18 @@
 <?php
 session_start();
 
-require_once 'FFAuctionConstants.php';
+require_once 'AuctionState.php';
 
-header('Content-type: application/json');
-print(file_get_contents(FFAuctionConstants::STATE_FILE_LOCATION));
-?>
+$ts = -1;
+if (isset($_REQUEST['timestamp'])) {
+	$ts = intval($_REQUEST['timestamp']);;
+}
+
+$auction_state = AuctionState::load();
+
+header("Content-type: application/json");
+if ($auction_state->getTimestamp() <= $ts) {
+	print(json_encode(false));
+} else {
+	print($auction_state->asJSON());
+}
